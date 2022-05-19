4fa489173dd71787b4f07e67b7620976a0986f25
Mikle Seregin <mikhail-seregin@yandex.ru>
2022-05-19 17:35:41 +0300

    /news/1/ - adaptive

--- js/script.js
+++ js/script.js
@@ -117,22 +117,24 @@ window.addEventListener('load', function () {
     }
 });
 window.addEventListener('load', function (){
-    let elem = document.querySelector('.article-more__swiper');
-    if (elem) {
-        new Swiper(elem, {
-            slidesPerView: 'auto',
-            speed: 500,
-            spaceBetween: 20,
-            loop: true,
-            centeredSlides: true,
-            grabCursor: true,
-            slideActiveClass: 'active',
-            navigation: {
-                nextEl: '.article-more__next',
-                prevEl: '.article-more__prev',
-            },
+    if (!isMobile.phone) {
+        let elem = document.querySelector('.article-more__swiper');
+        if (elem) {
+            new Swiper(elem, {
+                slidesPerView: 'auto',
+                speed: 500,
+                spaceBetween: 20,
+                loop: true,
+                centeredSlides: true,
+                grabCursor: true,
+                slideActiveClass: 'active',
+                navigation: {
+                    nextEl: '.article-more__next',
+                    prevEl: '.article-more__prev',
+                },
 
-        });
+            });
+        }
     }
 });
 window.addEventListener('load', function (){
